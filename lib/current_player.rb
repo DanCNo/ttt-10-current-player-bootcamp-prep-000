121d12b3efe9369bace243fa019f2487342ca35e
Dan N <dan.noh@gmail.com>
2018-08-29 21:18:27 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,9 +3,12 @@
 def turn_count(board)
   counter = 0 
   board.each do |index|
-    if index == "X" || index == "O"
-      counter += 1
-    end
+    #if index == "X" || index == "O"
+      #counter += 1
+    #end
+    
+    index == "X" || index == "O" ? counter += 1 : 
+    
   end
   return counter
 end
